1d2f71e58a00078068d8fae1d12372327a335d05
Firwanaa <65984781+Firwanaa@users.noreply.github.com>
2020-06-09 17:49:28 -0400

    Update TuesdayWeek3.java

--- src/tuesdayweek3/TuesdayWeek3.java
+++ src/tuesdayweek3/TuesdayWeek3.java
@@ -17,6 +17,7 @@ public class TuesdayWeek3 {
     public static void main(String[] args) {
         // TODO code application logic here
         System.out.println("learning Push Op");
+        System.out.println("Hello From Github");
         
         
     }
