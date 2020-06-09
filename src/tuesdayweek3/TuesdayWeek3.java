c0c9d1dbe79beec78465916852fe2d8d375b54e2
Firwanaa <65984781+Firwanaa@users.noreply.github.com>
2020-06-09 18:11:11 -0400

    Update TuesdayWeek3.java

--- src/tuesdayweek3/TuesdayWeek3.java
+++ src/tuesdayweek3/TuesdayWeek3.java
@@ -20,6 +20,7 @@ public class TuesdayWeek3 {
         System.out.println("Hello From Github pull-1");
          System.out.println("Hello From Github02 pull-2");
         System.out.println("Hello From Github03 Fetch");
+        System.out.println("Hello From Github03 Fetch 2");
         
     }
     
